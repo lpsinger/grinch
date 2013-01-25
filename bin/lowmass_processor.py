215fcee758009dff6e8805f6aacc31f70a473178
Chris Pankow <chris.pankow@ligo.org>
2013-01-24 18:41:08 -0600

    Copy over all the files from er1/box.

--- bin/lowmass_processor.py
+++ bin/lowmass_processor.py
@@ -4,47 +4,40 @@ import os
 import os.path
 import tempfile
 import urlparse
-import logging
 import re
 import ConfigParser
 
 from sys            import argv, exit, stdin
 
 from glue.ligolw    import ligolw
-from glue.ligolw    import utils as ligolwutils
-from glue.ligolw    import table as ligolwtable
+from glue.ligolw    import utils
+from glue.ligolw    import table
 from glue.ligolw    import lsctables
 
-## lowmass_processor logger
-logfile = 'logs/lowmass_processor.log'
-logger    = logging.getLogger('lowmass_processor')
-hdlr      = logging.FileHandler(logfile)
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-hdlr.setFormatter(formatter)
-logger.addHandler(hdlr)
-logger.setLevel(logging.DEBUG)
-logger.info('\nStarting lowmass_processor')
-
-## ligolw document
-doc      = ligolw.Document()
-handler  = ligolw.LIGOLWContentHandler(doc)
+## create dict from gracedb table
+doc        = ligolw.Document()
+handler    = ligolw.LIGOLWContentHandler(doc)
 ligolw.make_parser(handler).parse(stdin)
-lvatable = ligolwtable.get_table(doc,'LVAlert:table')
+lvatable   = table.get_table(doc,'LVAlert:table')
+
+raw_stream = lvatable.childNodes[5].pcdata.replace('"','').replace('\t','').replace('\n','').split(',')
+streamdata = {'alert_type'  : raw_stream[1],
+              'uid'         : raw_stream[2],
+              'file'        : raw_stream[3],
+              'description' : raw_stream[4]}
 
 ## if labeled EM_READY
-if lvatable[0].alert_type == 'label' and lvatable[0].description == 'EM_READY':
-    logger.info('Beginning EM_READY sequence')
+if streamdata['alert_type'] == 'label' and streamdata['description'] == 'EM_READY':
     import bayestar.lvalert
-    bayestar.lvalert.respond(lvatable[0].uid,submit=True)
+    bayestar.lvalert.respond(streamdata['uid'],submit=True)
     ## function ends processor
 
 ## check if new
-elif lvatable[0].alert_type == 'new':
-    logger.info('Beginning new event sequence')
+elif  streamdata['alert_type'] == 'new':
+    pass
 
 ## else
 else:
-     logger.info('Aborting lowmass_process, alert type %s not new or labeling EM_READY'%lvatable[0].alert_type)
      exit()
 
 ## read lowmass_config.ini
@@ -52,7 +45,7 @@ cp = ConfigParser.ConfigParser()
 cp.read('lowmass_config.ini')
 
 homedir            = os.getcwd()
-private_gracedir   = os.path.split(urlparse.urlparse(lvatable[0].file)[2])[0]
+private_gracedir   = os.path.split(urlparse.urlparse(streamdata['file'])[2])[0]
 general_gracedir   = private_gracedir.replace('private','general')
 processor_gracedir = "".join([general_gracedir,'/gdb_processor/'])
 
@@ -68,13 +61,13 @@ gridsfile          = cp.get('skypoints','gridsfile')
 coincdetconfig     = cp.get('coincdet','configfile')
 
 ## extract information about the event
-if re.search('.xml',lvatable[0].file):
-     coincfile = urlparse.urlparse(lvatable[0].file)[2]
-     logger.info('The coincidence file...%s'%coincfile)
+if re.search('.xml',streamdata['file']):
+     coincfile = urlparse.urlparse(streamdata['file'])[2]
 else:
      coincfile = "".join([private_gracedir,'/coinc.xml'])
-doc        = ligolwutils.load_filename(coincfile)
-coinctable = ligolwtable.get_table(doc,lsctables.CoincInspiralTable.tableName)
+doc        = utils.load_filename(coincfile)
+coinctable = table.get_table(doc,lsctables.CoincInspiralTable.tableName)
+
 gpstime    = coinctable[0].end_time
 ifonames   = ['H1','L1','V1']
 if 'test' in cp.sections():
@@ -110,7 +103,7 @@ Queue
 """
 tmplog  = tempfile.mkstemp()[1]
 subFile = open('skypoints.sub','w')
-subFile.write(contents%{'script':skypointscript,'coincfile':coincfile,'ranksfile':ranksfile,'gridsfile':gridsfile,'tmplog':tmplog,'uid':lvatable[0].uid})
+subFile.write(contents%{'script':skypointscript,'coincfile':coincfile,'ranksfile':ranksfile,'gridsfile':gridsfile,'tmplog':tmplog,'uid':streamdata['uid']})
 subFile.close()
 
 ## write data quality.sub
@@ -131,7 +124,7 @@ Queue
 """
 tmplog  = tempfile.mkstemp()[1]
 subFile = open('dq.sub','w')
-subFile.write(contents%{'tmplog':tmplog,'uid':lvatable[0].uid})
+subFile.write(contents%{'tmplog':tmplog,'uid':streamdata['uid']})
 subFile.close()
 
 ## write emlabel.sub
@@ -152,7 +145,7 @@ Queue
 """
 tmplog  = tempfile.mkstemp()[1]
 subFile = open('emlabel.sub','w')
-subFile.write(contents%{'script':dqtolabelscript,'gdbcommand':gracedbcommand,'vetodefinerfile':vetodefinerfile,'tmplog':tmplog,'uid':lvatable[0].uid})
+subFile.write(contents%{'script':dqtolabelscript,'gdbcommand':gracedbcommand,'vetodefinerfile':vetodefinerfile,'tmplog':tmplog,'uid':streamdata['uid']})
 subFile.close()
 
 ## write coincdet.sub
@@ -174,27 +167,27 @@ Queue
 """
 tmplog  = tempfile.mkstemp()[1]
 subFile = open('coincdet.sub','w')
-subFile.write(contents%{'script':coincdetscript,'coincfile':coincfile,'configfile':coincdetconfig,'tmplog':tmplog,'uid':lvatable[0].uid})
+subFile.write(contents%{'script':coincdetscript,'coincfile':coincfile,'configfile':coincdetconfig,'tmplog':tmplog,'uid':streamdata['uid']})
 subFile.close()
 
 ## write lowmass_runner.dag
 dagfile = "".join([processor_gracedir,'/lowmass_runner.dag'])
 f = open(dagfile,'w')
 f.write('JOB SKYPOINTS skypoints.sub\n')
-f.write(('SCRIPT PRE SKYPOINTS %s log %s Sky localization started\n' % (gracedbcommand, lvatable[0].uid)))
-f.write(('SCRIPT POST SKYPOINTS %s log %s Sky localization complete\n' % (gracedbcommand, lvatable[0].uid)))
+f.write(('SCRIPT PRE SKYPOINTS %s log %s Sky localization started\n' % (gracedbcommand, streamdata['uid'])))
+f.write(('SCRIPT POST SKYPOINTS %s log %s Sky localization complete\n' % (gracedbcommand, streamdata['uid'])))
 
 f.write('JOB DQ1 dq.sub\n')
 #f.write(('SCRIPT PRE DQ1 %s --gps-time %s --ifos %s\n' % (dqwaitscript,itime,ifos)))
 #f.write(('SCRIPT POST DQ1 %s -f %s -i %s -g %s --veto-definer-file %s --ifos %s\n' % (dqtolabelscript,'dq.xml',lvatable[0].uid,gracedbcommand,vetodefinerfile,ifos)))
 
 f.write('JOB EMLABEL emlabel.sub\n')
-f.write(('SCRIPT PRE EMLABEL %s log %s EM_READY labeling started  \n' % (gracedbcommand, lvatable[0].uid)))
-f.write(('SCRIPT POST EMLABEL %s log %s EM_READY labeling complete\n' % (gracedbcommand, lvatable[0].uid)))
+f.write(('SCRIPT PRE EMLABEL %s log %s EM_READY labeling started  \n' % (gracedbcommand, streamdata['uid'])))
+f.write(('SCRIPT POST EMLABEL %s log %s EM_READY labeling complete\n' % (gracedbcommand, streamdata['uid'])))
 
 f.write('JOB COINCDET coincdet.sub\n')
-f.write(('SCRIPT PRE COINCDET %s log %s Coincidence search started \n' % (gracedbcommand, lvatable[0].uid)))
-f.write(('SCRIPT POST COINCDET %s log %s Coincidence search complete \n' % (gracedbcommand, lvatable[0].uid)))
+f.write(('SCRIPT PRE COINCDET %s log %s Coincidence search started \n' % (gracedbcommand, streamdata['uid'])))
+f.write(('SCRIPT POST COINCDET %s log %s Coincidence search complete \n' % (gracedbcommand, streamdata['uid'])))
 
 f.write('PARENT DQ1 CHILD EMLABEL\n')
 f.write('PARENT SKYPOINTS CHILD EMLABEL\n')
@@ -202,8 +195,6 @@ f.write('PARENT SKYPOINTS CHILD COINCDET\n')
 f.close()
 
 ## submit dag
-logger.info('Submitting DAG')
 os.chdir(processor_gracedir)
 condorargs=['condor_submit_dag','lowmass_runner.dag']
-logger.info('Completed new event sequence\n')
 os.execlp('condor_submit_dag', *condorargs)
