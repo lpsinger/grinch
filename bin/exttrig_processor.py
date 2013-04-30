64e8a2aa0132a6eebe1ec8db3a5a957dbdacf4eb
Alex Urban <alexander.urban@ligo.org>
2013-04-30 18:46:40 -0500

    exttrig_processor now gets launched by an LV alert; will launch coincidence search via a condor dag

--- bin/exttrig_processor.py
+++ bin/exttrig_processor.py
@@ -1,67 +1,38 @@
 #!/usr/bin/env python
 
-"""
-Script to execute temporal coincidence search 
-for  external triggers
-"""
 __author__ = "Alex Urban <alexander.urban@ligo.org>"
 
 import os
 
-from GRB import gracedb, ExtTrig
-from optparse import Option, OptionParser
+from sys import stdin, exit
+from ligo.gracedb.rest import GraceDb
+from ligo.lvalert.utils import get_LVAdata_from_stdin
 
 
-# read in options from command line
-opts, args = OptionParser(
-    description = __doc__,
-    usage = "%prog [options] [INPUT]",
-    option_list = [
-        Option("-g","--graceid",help="graceid of external trigger event"),
-        Option("-x","--xml",metavar="FILE.xml",help="VOEvent .xml file of external trigger")
-    ]
-).parse_args()
+# initialize instance of gracedb rest API
+gracedb = GraceDb()
+
+# create dictionary from gracedb table
+streamdata = get_LVAdata_from_stdin(stdin, as_dict=True)
+
+# test whether this event is new
+if streamdata['alert_type'] == 'new':
+    pass
+else:
+     exit()
 
 # create working directory for the trigger
 home = os.getenv("HOME")
-working = home + '/working/ExtTrig/' + opts.graceid
-test = os.path.exists(working)
-if test == False: os.mkdir(working)
+working = home + '/working/ExtTrig/%s' % streamdata['uid']
+try:
+    os.mkdir(working)
+except OSError:
+    print 'Could not make directory %s' % working
+    pass
 
-# change current working directory and download VOEvent .xml file
+# change current working directory
 os.chdir(working)
-gracedb.files(opts.graceid,filename=opts.xml)
-trig = ExtTrig(opts.trigger,opts.xml)
-trig.write_fits()
 
-# inform GraCEDb things are about to go down
-note = 'Initiating coincidence search'
-trig.submit_gracedb_log(note)
-
-
-#####################################
-# initiate the coincidence searches #
-#####################################
-
-# first, the short-duration search
-coincs = trig.short_search()
-if coincs != []: # produce plots and skymaps if there is a non-null result
-    from GW import GraCE
-    for i in xrange(len(coincs)):
-        trigdir = os.getcwd() + '/'
-        trigfits = trigdir + trig.fits # will need the path to this file
-
-        working2 = home + '/working/GW/' + coincs[i][0]
-        test2 = os.path.exists(working2)
-        if test2 == False: os.mkdir(working2)
-        os.chdir(working2) # move to GW event working directory
-
-        event = GraCE(coincs[i][0]) # initialize object of class GW
-        event.plot_trig(trigfits) # plot and upload skymap with trigger
-        event.plot_xcor(trigfits) # plot and upload cross-correlation
-
-# finally, the long-duration search
-coincs2 = trig.long_search()
-
-note2 = 'Coincidence search complete'
-trig.submit_gracedb_log(note2)
+# get info
+xml = streamdata['file'] # CLEAN THIS UP; THIS IS WHERE YOU LEFT OFF
+gracedb.files(streamdata['uid'],filename=xml)
