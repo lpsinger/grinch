847fb5475045a4d8afd19c9f9e3f97009e234c22
Alex Urban <alexander.urban@ligo.org>
2013-05-01 13:58:54 -0500

    Move GRB and GW modules into a package called gracedb_events

--- setup.py
+++ setup.py
@@ -26,10 +26,11 @@ from distutils.core import setup
 
 setup(
     name='gdb-processor',
-    version='1.0',
+    version='0.1',
+    url='http://gracedb.ligo.org',
     author='Alex Urban',
     author_email='alexander.urban@ligo.org',
-    description='Coordinate between LVAlerts, gracedb, and condor job submission automatically',
+    description='Coordinate between LV alerts, gracedb, and condor job submission automatically; for specifics see README',
     license='GNU General Public License Version 3',
     scripts=[
         'gdb_processor/bin/lowmass_processor.py',
@@ -37,11 +38,15 @@ setup(
         'gdb_processor/bin/dqtolabel.py',
         'gdb_processor/bin/GRB.py',
         'gdb_processor/bin/GW.py',
+        'gdb_processor/bin/workflow_helper.py',
         'gdb_processor/bin/lvalertlisten',
         'gdb_processor/bin/plot_allsky',
         'gdb_processor/bin/plot_xcorrelate',
+        'gdb_processor/bin/coinc_search',
         'gdb_processor/bin/lowmass_processor_test.py',
         'gdb_processor/etc/lowmass_config.ini',
+        'gdb_processor/etc/exttrig_config.ini',
+        'gdb_processor/etc/gw_config.ini',
         'gdb_processor/etc/coincdet.ini',
         'gdb_processor/etc/lvalertconfig.ini',
         'gdb_processor/etc/lvalertlisten.sub'
